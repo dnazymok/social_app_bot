142dfa86138decbdf601e0a68b569fa981e86da9
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-05 16:30:14 +0300

    fix self attributes naming in UserApiClient

--- core/user_api.py
+++ core/user_api.py
@@ -7,23 +7,23 @@ from requests import HTTPError
 
 class UserApiClient:
     def __init__(self, user, post_factory: Type[BasePostFactory]):
-        self.user = user
-        self.client = clients.get_client()
+        self._user = user
+        self._client = clients.get_client()
         self._post_data = post_factory().make_post()
-        self._register_data = {'email': self.user.email,
-                               'username': self.user.username,
-                               'password': self.user.password}
-        self._login_data = {'username': self.user.username,
-                            'password': self.user.password}
+        self._register_data = {'email': self._user.email,
+                               'username': self._user.username,
+                               'password': self._user.password}
+        self._login_data = {'username': self._user.username,
+                            'password': self._user.password}
         self._token = ''
 
     def register(self):
         try:
-            self.client.post_request('users/', data=self._register_data)
+            self._client.post_request('users/', data=self._register_data)
         except HTTPError:  # todo custom error from clients.py
-            logging.info(f'{self.user.username} already registered.')
+            logging.info(f'{self._user.username} already registered.')
         else:
-            logging.info(f'{self.user.username} registered.')
+            logging.info(f'{self._user.username} registered.')
 
     def login(self):
         if not self._token:
@@ -33,26 +33,26 @@ class UserApiClient:
         self._token = ''
 
     def create_post(self):
-        self.client.post_request('posts/', data=self._post_data,
-                                 headers=self._auth_headers)
-        logging.info(f'{self.user.username} created post.')
+        self._client.post_request('posts/', data=self._post_data,
+                                  headers=self._auth_headers)
+        logging.info(f'{self._user.username} created post.')
 
     def like_post(self, post_id):
         try:
-            self.client.post_request(f'posts/{post_id}/likes',
-                                     data=self._post_data,
-                                     headers=self._auth_headers)
+            self._client.post_request(f'posts/{post_id}/likes',
+                                      data=self._post_data,
+                                      headers=self._auth_headers)
         except HTTPError:  # todo custom error
-            logging.info(f'{self.user.username} already liked post {post_id}')
+            logging.info(f'{self._user.username} already liked post {post_id}')
         else:
-            logging.info(f'{self.user.username} liked post {post_id}')
+            logging.info(f'{self._user.username} liked post {post_id}')
 
     def _get_access_token(self):
-        response = self.client.post_request('token/', data=self._login_data)
+        response = self._client.post_request('token/', data=self._login_data)
         return response.json()['access']
 
     def _get_refresh_token(self):
-        response = self.client.post_request('token/', data=self._login_data)
+        response = self._client.post_request('token/', data=self._login_data)
         return response.json()['refresh']
 
     @property
