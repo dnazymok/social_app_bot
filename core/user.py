38c31281ffc9bb0cd9a52981becb81c4b5bf3b65
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-06 12:23:21 +0300

    remove register and login data from UserApiClient

--- core/user.py
+++ core/user.py
@@ -8,3 +8,14 @@ class User:
         self.password = password
         self.email = email
         self.api = UserApiClient(self, FakePostFactory)
+
+    @property
+    def register_data(self):
+        return {'email': self.email,
+                'username': self.username,
+                'password': self.password}
+
+    @property
+    def login_data(self):
+        return {'username': self.username,
+                'password': self.password}
--- core/user_api.py
+++ core/user_api.py
@@ -9,15 +9,10 @@ class UserApiClient:
         self._user = user
         self._client = clients.get_client()
         self._post_data = post_factory().make_post()
-        self._register_data = {'email': self._user.email,
-                               'username': self._user.username,
-                               'password': self._user.password}
-        self._login_data = {'username': self._user.username,
-                            'password': self._user.password}
         self._token = ''
 
     def register(self):
-        self._client.post_request('users/', data=self._register_data)
+        self._client.post_request('users/', data=self._user.register_data)
         logging.info(f'{self._user.username} registered.')
 
     def login(self):
@@ -39,11 +34,11 @@ class UserApiClient:
         logging.info(f'{self._user.username} liked post {post_id}')
 
     def _get_access_token(self):
-        response = self._client.post_request('token/', data=self._login_data)
+        response = self._client.post_request('token/', data=self._user.login_data)
         return response.json()['access']
 
     def _get_refresh_token(self):
-        response = self._client.post_request('token/', data=self._login_data)
+        response = self._client.post_request('token/', data=self._user.login_data)
         return response.json()['refresh']
 
     @property
