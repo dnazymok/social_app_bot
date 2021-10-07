211d0f937ba16aa3bc69770ad598631c62c9e8ca
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-07 14:33:31 +0300

    add type hinting for LikeGeneratorService

--- core/services.py
+++ core/services.py
@@ -1,11 +1,11 @@
 import random
 
 from user import User
-from post import Like
+from post import Post, Like
 
 
 class LikesGeneratorService:
-    def __init__(self, users, max_likes):
+    def __init__(self, users: [User], max_likes: int):
         self._users = users
         self._max_likes = max_likes
 
@@ -33,31 +33,31 @@ class LikesGeneratorService:
                     if post in posts_to_like:
                         posts_to_like.remove(post)
 
-    def _sort_users_by_posts_count(self, users):
+    def _sort_users_by_posts_count(self, users: [User]) -> [User]:
         return sorted(users, key=lambda user: user.posts_count, reverse=True)
 
-    def _get_users_with_zero_liked_post(self, users: [User]):
+    def _get_users_with_zero_liked_post(self, users: [User]) -> [User]:
         users_with_zero_liked_post = []
         for user in users:
             if user.is_zero_liked_post:
                 users_with_zero_liked_post.append(user)
         return users_with_zero_liked_post
 
-    def _get_posts_from_users(self, users: [User]):
+    def _get_posts_from_users(self, users: [User]) -> [Post]:
         posts = []
         for user in users:
             for post in user.posts:
                 posts.append(post)
         return posts
 
-    def _get_all_posts(self, users):
+    def _get_all_posts(self, users: [User]) -> [Post]:
         posts = []
         for user in users:
             for post in user.posts:
                 posts.append(post)
         return posts
 
-    def _get_post_by_id(self, posts, _id):
+    def _get_post_by_id(self, posts: [Post], _id: int) -> Post:
         for post in posts:
             if post.id == _id:
                 return post
