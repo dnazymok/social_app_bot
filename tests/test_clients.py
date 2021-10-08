e6536ebfc0ff9121d9e62784204b03a5f34186a0
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-08 14:35:51 +0300

    add tests for login/logout UserApiClient

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,8 +1,7 @@
 import pytest
-import requests
 
-from core.clients import ApiClient
-from core.config import Config
+from core.clients import ApiClient, UserApiClient
+from unittest.mock import Mock
 
 
 class TestApiClient:
@@ -17,7 +16,7 @@ class TestApiClient:
     @pytest.mark.parametrize('path',
                              ['/posts', '/comments', '/albums', '/photos'])
     def test_post_request(self, api_client, path):
-        response = api_client.post_request(path=path)
+        response = api_client.post_request(path=path, data=self.post_data)
         assert response.status_code == 201
 
     @pytest.mark.parametrize('path',
@@ -25,3 +24,24 @@ class TestApiClient:
     def test_get_request(self, api_client, path):
         response = api_client.get_request(path=path)
         assert response.status_code == 200
+
+
+class TestUserApiClient:
+    @pytest.fixture()
+    def user(self):
+        return Mock()
+
+    @pytest.fixture()
+    def api_client(self, user):
+        return UserApiClient(user)
+
+    def test_login(self, api_client):
+        get_access_token_mock = Mock(return_value='token')
+        api_client._get_access_token = get_access_token_mock
+        api_client.login()
+        assert api_client._token == 'token'
+
+    def test_logout(self, api_client):
+        api_client._token = 'token'
+        api_client.logout()
+        assert api_client._token == ''
