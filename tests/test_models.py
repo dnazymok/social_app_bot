d417beb09fc8b39638be5255508de5ba61ecbf92
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-11 11:20:42 +0300

    add test_is_zero_liked_post to test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from core.models import User, Like, Post
@@ -12,7 +14,21 @@ class TestUser:
     def post(self):
         return Post('title', 'descr', 'content')
 
+    @pytest.fixture()
+    def post_with_like(self):
+        like = Mock()
+        post = Post('title', 'descr', 'content')
+        post.likes.append(like)
+        return post
+
     def test_posts_count(self, user, post):
         assert user.posts_count == 0
         user.posts.append(post)
         assert user.posts_count == 1
+
+    def test_is_zero_liked_post(self, user, post, post_with_like):
+        user.posts.append(post)
+        assert user.is_zero_liked_post
+        user.posts.pop()
+        user.posts.append(post_with_like)
+        assert not user.is_zero_liked_post
