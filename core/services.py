d31548e3b01ed48d8ede45d7ace79eb824dc5f42
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-15 15:48:37 +0300

    refactor start method

--- core/services.py
+++ core/services.py
@@ -15,22 +15,19 @@ class LikesGeneratorService:
         for user in users_by_posts_count:
             users_with_zero_liked_post = self._get_users_with_zero_liked_post(
                 self._users)
-            if not users_with_zero_liked_post:
-                break
-            if user in users_with_zero_liked_post:
-                users_with_zero_liked_post.remove(user)
-            posts_to_like = self._get_posts_from_users(
-                users_with_zero_liked_post)
-            for i in range(self._max_likes):
-                if posts_to_like:
-                    post = random.choice(posts_to_like)
-                    response = user.api.like_post(post.id)
-                    if response.status_code == 201:
-                        self._get_post_by_id(all_posts, post.id)
-                        like = Like(user=user, post=post)
-                        post.likes.append(like)
-                    if post in posts_to_like:
-                        posts_to_like.remove(post)
+            if users_with_zero_liked_post:
+                self._start_user_like_process(
+                    user,
+                    users_with_zero_liked_post,
+                    all_posts
+                )
+
+    def _get_posts_from_users(self, users: [User]) -> [Post]:
+        posts = []
+        for user in users:
+            for post in user.posts:
+                posts.append(post)
+        return posts
 
     def _sort_users_by_posts_count(self, users: [User]) -> [User]:
         return sorted(users, key=lambda user: user.posts_count, reverse=True)
@@ -42,12 +39,28 @@ class LikesGeneratorService:
                 users_with_zero_liked_post.append(user)
         return users_with_zero_liked_post
 
-    def _get_posts_from_users(self, users: [User]) -> [Post]:
-        posts = []
-        for user in users:
-            for post in user.posts:
-                posts.append(post)
-        return posts
+    def _start_user_like_process(self, user, users_with_zero_liked_post,
+                                 all_posts):
+        if user in users_with_zero_liked_post:
+            users_with_zero_liked_post.remove(user)
+        posts_to_like = self._get_posts_from_users(
+            users_with_zero_liked_post)
+        for i in range(self._max_likes):
+            if posts_to_like:
+                self._like_post(user, posts_to_like, all_posts)
+
+    def _like_post(self, user, posts_to_like, all_posts):
+        post = random.choice(posts_to_like)
+        response = user.api.like_post(post.id)
+        if response.status_code == 201:
+            self._append_like_to_post(user, all_posts, post)
+        if post in posts_to_like:
+            posts_to_like.remove(post)
+
+    def _append_like_to_post(self, user, all_posts, post):
+        self._get_post_by_id(all_posts, post.id)
+        like = Like(user=user, post=post)
+        post.likes.append(like)
 
     def _get_post_by_id(self, posts: [Post], _id: int) -> Post:
         for post in posts:
